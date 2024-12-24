fe612f42ac9efcbca0f5b7fce575338b0b9ffd2d
Anderson Lima <anderson.gadelha@zup.com.br>
2024-12-24 10:10:45 -0300

    docs: adiciona java doc a classe InputUtil

--- src/util/InputUtil.java
+++ src/util/InputUtil.java
@@ -2,8 +2,21 @@ package util;
 
 import java.util.Scanner;
 
+/**
+ *  Input Util
+ *
+ *  <p> Essa classe util tem como objetivo auxiliar na entrada de inputs</p>
+ *
+ * @author Anderson Gadelha
+ */
 public class InputUtil {
 
+    /**
+     * Função para retornar um inteiro e retornar valor default caso seja recebido algum valor diferente de um inteiro.
+     *
+     * @param scanner
+     * @retur um valor inteiro, tendo zero como default.
+     */
     public static int getOpcaoInteira(Scanner scanner) {
         String entrada = scanner.nextLine();
         int entradaTratada = 0;
